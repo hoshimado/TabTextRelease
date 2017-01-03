a0a8c423512664804d21e613d145c4bb873af182
xingyanhuan <xingyanhuan@yahoo.co.jp>
2017-01-03 19:40:19 +0900

    TabText239
    両手たぶメモ   Version 2.39   [TabText23.zip]
    リリース日：2017.01.03
    
    Zipモジュールだけじゃなく、自動更新用のモジュールも合わせてGitで管理してみる。

--- copy_without_git.bat
+++ copy_without_git.bat
@@ -1,1 +1,9 @@
 xcopy .\TabText_Extracted\* G:\TabText23x\  /S /E /EXCLUDE:xcopy-excludelist.txt 
+copy /Y .\TabText_Extracted\InstructionManual\History.txt              .\autoupdate_src\
+copy /Y .\TabText_Extracted\InstructionManual\PlugInSpecification.txt  .\autoupdate_src\
+copy /Y .\TabText_Extracted\InstructionManual\Read me.txt              .\autoupdate_src\
+copy /Y .\TabText_Extracted\TabTextList.def   .\autoupdate_src\
+copy /Y .\TabText_Extracted\Plugins\AppCallListNew.xml    .\autoupdate_src\
+copy /Y .\TabText_Extracted\Plugins\TtlPlgOffiExApp.tpi   .\autoupdate_src\
+copy /Y .\TabText_Extracted\Plugins\TtlPlgOffiNwGet.tpi   .\autoupdate_src\
+start G:\
